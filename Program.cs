3bdaf81329255435121d6df0e57d362b17a3d868
MuhammetUzun <105195447+MuhammetUzun@users.noreply.github.com>
2022-08-07 17:13:07 +0300

    NestedWhileLoop

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Döngü_Yapısı_2_While_İç_İçe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // iç içe while döngüsü ile çarpım tablosu
+            int sayi1 = 1;
+            int sayi2 = 1; 
+
+            while(sayi1 <=10)
+            {
+                while(sayi2 <= 10)
+                {
+                    Console.WriteLine(sayi1 + "*" + sayi2 + " = " + sayi1 * sayi2);
+                    sayi2++;
+                }
+                sayi1++;
+                sayi2 = 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
